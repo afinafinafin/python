50e60f8a51e7578d7302a45a4a7b33cb13029479
Afin <147811352+afinafinafin@users.noreply.github.com>
2026-02-13 12:17:02 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,25 @@
+class Calculator:
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
+
+    def divide(self, a, b):
+        if b == 0:
+            return "Cannot divide by zero"
+        return a / b
+
+
+calc = Calculator()
+
+a = float(input("\nEnter first number: "))
+b = float(input("Enter second number: "))
+
+print("Addition:", calc.add(a, b))
+print("Subtraction:", calc.subtract(a, b))
+print("Multiplication:", calc.multiply(a, b))
+print("Division:", calc.divide(a, b))
